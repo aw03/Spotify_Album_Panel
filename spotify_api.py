b8d67ca153b75d2127be148fdb79ecb9157dd0fe
aw03 <awu245@gmail.com>
2025-05-25 00:46:37 -0400

    Working grab token

---
+++ spotify_api.py
@@ -0,0 +1,35 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+token_url = "https://accounts.spotify.com/api/token"
+token_headers = {
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+token_data = {
+    "grant_type": "client_credentials"
+}
+
+response = requests.post(
+    token_url,
+    headers=token_headers,
+    data=token_data,
+    auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
+)
+
+# Get the JSON response
+response_json = response.json()
+
+# Extract the access token and token type
+access_token = response_json.get("access_token")
+token_type = response_json.get("token_type")
+
+headers = {
+    "Authorization": f"{token_type} {access_token}"
+}
+
