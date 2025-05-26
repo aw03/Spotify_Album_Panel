97f60ee84622160b9dd5c2ba5ae6b7b783625f11
aw03 <awu245@gmail.com>
2025-05-26 10:57:30 -0400

    api needs auth

--- spotify_api.py
+++ spotify_api.py
@@ -7,29 +7,48 @@ load_dotenv()
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-token_url = "https://accounts.spotify.com/api/token"
-token_headers = {
-    "Content-Type": "application/x-www-form-urlencoded"
-}
-token_data = {
-    "grant_type": "client_credentials"
-}
+token_header = None
 
-response = requests.post(
-    token_url,
-    headers=token_headers,
-    data=token_data,
-    auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
-)
+def get_token_header():
+    token_url = "https://accounts.spotify.com/api/token"
+    token_headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    token_data = {
+        "grant_type": "client_credentials"
+    }
 
-# Get the JSON response
-response_json = response.json()
+    response = requests.post(
+        token_url,
+        headers=token_headers,
+        data=token_data,
+        auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
+    )
 
-# Extract the access token and token type
-access_token = response_json.get("access_token")
-token_type = response_json.get("token_type")
+    # Get the JSON response
+    response_json = response.json()
 
-headers = {
-    "Authorization": f"{token_type} {access_token}"
-}
+    # Extract the access token and token type
+    access_token = response_json.get("access_token")
+    token_type = response_json.get("token_type")
+
+    headers = {
+        "Authorization": f"{token_type} {access_token}"
+    }
+
+    return headers
+
+def get_recently_played_tracks():
+    try:
+        response = requests.get(
+            "https://api.spotify.com/v1/me/player/recently-played",
+            headers=token_header)
+    except:
+        token_header = get_token_header()
+        response = requests.get(
+            "https://api.spotify.com/v1/me/player/recently-played",
+            headers=token_header)
+    return response
+
+print(get_recently_played_tracks().reason)
 
